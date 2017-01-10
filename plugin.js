b2d2a8492e129903b2ed342b2c4a87125dc25a0a
flexage <flexage@gmail.com>
2017-01-10 22:29:40 +0000

    Updated: Basic Plugin Info

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Flextuary Skin';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'flextuaryskin';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'flextuaryskin',
+        name: 'Flextuary Skin'
     };
 });
