aa38df46396f8d4284325dc59fd03703cb2532ff
flexage <flexage@gmail.com>
2017-05-04 21:16:18 +0100

    Added: Item Info to panel-left on Movies View

--- components/tabbedpagelibrary.js
+++ components/tabbedpagelibrary.js
@@ -1,5 +1,7 @@
-define(['connectionManager', 'loading', 'scroller', './focushandler', 'focusManager', 'scrollHelper', 'browser', './../skininfo', 'emby-button', 'scrollStyles'], function (connectionManager, loading, scroller, focusHandler, focusManager, scrollHelper, browser, skinInfo) {
+define(['connectionManager', 'imageLoader', 'loading', 'scroller', './focushandler', 'focusManager', 'scrollHelper', 'browser', './../skininfo', 'emby-button', 'scrollStyles'], function (connectionManager, imageLoader, loading, scroller, focusHandler, focusManager, scrollHelper, browser, skinInfo) {
     'use strict';
+
+    var apiClient = connectionManager.currentApiClient();
 
     function focusViewSlider() {
 
@@ -130,6 +132,270 @@ define(['connectionManager', 'loading', 'scroller', './focushandler', 'focusMana
                 fadeInRight(contentScrollSlider);
             });
         }
+    }
+
+    function libraryItemFocus(e) {
+        var oldSelectedItem = document.querySelector('.selectedItemId')
+        var oldSelectedItemId = '';
+        if(oldSelectedItem) {
+            oldSelectedItemId = oldSelectedItem.getAttribute('data-selected-id');
+        }
+
+        // Build Item Info Pane
+        var itemInfoElement = document.querySelector('.itemInfo');
+        var selectedIndexElement = document.querySelector('.selectedIndex');
+        var focused = focusManager.focusableParent(e.target);
+
+        var itemId = focused.getAttribute('data-id');
+
+        if(itemId === oldSelectedItemId) return;
+
+        Emby.Models.item(itemId).then(function (item) {
+            console.log('itemResult', item);
+
+            var imageUrlPrimary = apiClient.getImageUrl(item.Id, {
+                type: "Primary",
+                maxHeight: 400,
+                maxWidth: 300,
+                tag: item.ImageTags.Primary
+            });
+
+            var imageUrlBackdrop = apiClient.getImageUrl(item.Id, {
+                type: "Backdrop",
+                maxHeight: 1920,
+                maxWidth: 1080,
+                tag: item.BackDropImageTags
+            });
+
+            var html = '';
+            html += '<div class="selectedItemId hide" data-selected-id="' + itemId + '"></div>'
+            html += '<div class="primary lazy" data-src="' + imageUrlPrimary + '"></div>';
+            html += '<div class="backdrop lazy" data-src="' + imageUrlBackdrop + '"></div>';
+
+            html += '<div class="textInfo">';
+            html += '<h1 class="name">' + item.Name + '</h1>';
+            html += '<h1 class="year">' + item.ProductionYear + '</h1>';
+
+            if(item.Genres.length > 0){
+                html += '<h2 class="genres">' + item.Genres.join(' / ') + '</h2>';
+            }
+
+            html += '<p class="overview">' + item.Overview + '</p>';
+            html += '</div>';
+            if(item.CommunityRating) {
+                html += '<div class="communityRating"><span class="rating">' + item.CommunityRating + '</span><span divider>/</span>10</div>';
+            }
+
+            if(item.RunTimeTicks)
+            {
+                var runTime = Math.ceil((item.RunTimeTicks / 10000) / 60000);
+                html += '<div class="runTime">' + runTime + ' minutes</div>';
+            }
+
+            // var videoStream = (item.MediaStreams || []).filter(function (i) {
+            //     return i.Type == 'Video';
+            // })[0] || {};
+            // var audioStream = (item.MediaStreams || []).filter(function (i) {
+            //     return i.Type == 'Audio';
+            // })[0] || {};
+            //
+            // console.log('videoStream', videoStream);
+            // console.log('audioStream', audioStream);
+            //
+            // if(videoStream) {
+            //     html += '';
+            // }
+
+            html += '<div class="mediaInfoPrimary">';
+            var mediaSource = item.MediaSources[0];
+
+            var videoStream = (mediaSource.MediaStreams || []).filter(function (i) {
+                return i.Type == 'Video';
+            })[0] || {};
+            var audioStream = (mediaSource.MediaStreams || []).filter(function (i) {
+                return i.Type == 'Audio';
+            })[0] || {};
+
+            var resolutionText = getResolutionText(item);
+            if (resolutionText) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + resolutionText + '</div>';
+            }
+
+            var channels = getChannels(item);
+            var channelText;
+
+            if (channels == 8) {
+
+                channelText = '7.1';
+
+            } else if (channels == 7) {
+
+                channelText = '6.1';
+
+            } else if (channels == 6) {
+
+                channelText = '5.1';
+
+            } else if (channels == 2) {
+
+                channelText = '2.0';
+            }
+
+            if (channelText) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + channelText + '</div>';
+            }
+
+            html += '</div>';
+
+            html += '<div class="mediaInfo">';
+
+            if (mediaSource.Container) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + mediaSource.Container + '</div>';
+            }
+
+            if (videoStream.Codec) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + videoStream.Codec + '</div>';
+            }
+
+            if (audioStream.Codec == 'dca' && audioStream.Profile) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + audioStream.Profile + '</div>';
+            } else if (audioStream.Codec) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + audioStream.Codec + '</div>';
+            }
+
+            if (videoStream.AspectRatio) {
+                html += '<div class="mediaInfoIcon mediaInfoText">' + videoStream.AspectRatio + '</div>';
+            }
+
+            html += '</div>';
+
+            itemInfoElement.innerHTML = html;
+
+            imageLoader.lazyChildren(itemInfoElement);
+
+
+            var index = focused.getAttribute('data-index');
+            if (index) {
+                selectedIndexElement.innerHTML = 1 + parseInt(index);
+            }
+
+            var overviewElement = itemInfoElement.querySelector('.overview');
+
+            var overviewHeight = overviewElement.scrollHeight;
+            console.log('overviewElement.clientHeight', overviewElement.clientHeight);
+            console.log('overviewElement.scrollHeight', overviewElement.scrollHeight);
+
+            var scrollTime = 5000;
+
+            //initOverviewScroll(overviewElement, overviewHeight, scrollTime);
+
+            setTimeout(function () {
+                scrollOverview(overviewElement);
+            }, 3000);
+
+
+            // // Selected Menu
+            // var selectedMenu = document.querySelector('.selectedMenu');
+            //
+            // var html = '';
+            // if(item.UserData.PlaybackPositionTicks > 0)
+            // {
+            //     // Convert ticks to hours and minutes
+            //     var positionTime = Math.ceil((item.UserData.PlaybackPositionTicks / 10000) / 60000);
+            //
+            //     html += '<button class="playbackResume" data-item-id="' + itemId + '">Resume from ' + positionTime + ' minutes</button>';
+            // }
+            //
+            // html += '<button class="playbackPlay" data-item-id="' + itemId + '">Play</button>';
+            //
+            // html += '<button class="moreInfo" data-item-id="' + itemId + '">More info</button>';
+            //
+            // if(item.LocalTrailerCount > 0)
+            // {
+            //     html += '<button class="playbackTrailer" data-item-id="' + itemId + '">Trailer</button>';
+            // }
+            //
+            // html += '<button>Mark as watched</button>';
+            //
+            // selectedMenu.innerHTML = html;
+            //
+            // bindSelectedMenuEvents(selectedMenu);
+        });
+    }
+
+    function scrollOverview(overviewElement)
+      {
+          var currentPosition = overviewElement.scrollTop;
+          var elementScrollHeight = overviewElement.scrollHeight;
+          var elementClientHeight = overviewElement.clientHeight;
+
+          if(currentPosition >= (elementScrollHeight - elementClientHeight) - 1) {
+              setTimeout(function() {
+                  overviewElement.scrollTop = 0;
+                  setTimeout(function () {
+                      scrollOverview(overviewElement);
+                  }, 3000);
+              }, 4000);
+              return;
+          }
+
+          overviewElement.scrollTop += 1;
+
+          setTimeout(function() {
+              scrollOverview(overviewElement);
+          }, 75);
+      }
+
+    function getResolutionText(item) {
+
+        if (!item.MediaSources || !item.MediaSources.length) {
+            return null;
+        }
+
+        return item.MediaSources[0].MediaStreams.filter(function (i) {
+
+            return i.Type == 'Video';
+
+        }).map(function (i) {
+
+            if (i.Height) {
+
+                if (i.Width >= 4000) {
+                    return '4K';
+                }
+                if (i.Width >= 2500) {
+                    return '1440P';
+                }
+                if (i.Width >= 1900) {
+                    return '1080P';
+                }
+                if (i.Width >= 1260) {
+                    return '720P';
+                }
+                if (i.Width >= 700) {
+                    return '480P';
+                }
+
+            }
+            return null;
+        })[0];
+
+    }
+
+    function getChannels(item) {
+
+        if (!item.MediaSources || !item.MediaSources.length) {
+            return 0;
+        }
+
+        return item.MediaSources[0].MediaStreams.filter(function (i) {
+
+            return i.Type == 'Audio';
+
+        }).map(function (i) {
+            return i.Channels;
+        })[0];
+
     }
 
     function fadeInRight(elem) {
@@ -314,6 +580,8 @@ define(['connectionManager', 'loading', 'scroller', './focushandler', 'focusMana
                     enableBackdrops: true
                 });
             }
+
+            view.addEventListener('focus', libraryItemFocus, true);
         }
 
         self.destroy = function () {
